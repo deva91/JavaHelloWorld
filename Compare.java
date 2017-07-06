1ff83a40985bf1c3e7a9f62162e84ac84b214d2d
deva91 <ewachalupniczak@gmail.com>
2017-07-06 21:58:03 +0200

    Update Compare.java

--- Compare.java
+++ Compare.java
@@ -1,27 +1,13 @@
-import java.util.Random;
-
 public class Compare 
 {
 	public static void main(String[] args) 
 	{
-		// create a random array
-		int size = 50;
+		// generate a random array
+		int size = 25;
 		int[] unsorted = new int[size];
-		for(int i=0;i<size;i++)
-			{
-			Random x = new Random();
-			unsorted[i] = x.nextInt(1000);
-			}
-		System.out.println("We have an unsorted array of " + size + " random elements of value between 0 and 1000");
 		
-		// display unsorted array if not more than 50 elements
-		if(size<=50)
-		{
-			for(int j : unsorted)
-				System.out.print(j + " ");
-			System.out.println();
-		}
-		System.out.println("Let's sort it.\n");
+		ArrayGenerator randomArray = new ArrayGenerator(size);
+		unsorted = randomArray.getAndPrintArray();
 		
 		//create object that will be sorted with Insertion Sort
 		InsertionSorter sortIS = new InsertionSorter(unsorted,size);
@@ -33,6 +19,6 @@ public class Compare
 		
 		//create object that will be sorted with Heap Sort
 		HeapSorter sortHS = new HeapSorter(unsorted,size);
-		sortHS.measureSortingTimeDisplayResults();		
+		sortHS.measureSortingTimeDisplayResults();	
 	}
 }
