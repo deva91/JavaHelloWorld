bee7c8e0ecd01a144e830ef3f39b6e9972e294dc
deva91 <ewachalupniczak@gmail.com>
2017-07-06 21:16:19 +0200

    Create InsertionSorter.java

---
+++ InsertionSorter.java
@@ -0,0 +1,23 @@
+public class InsertionSorter extends Sorter
+{
+	public InsertionSorter(int[] unsortedIS, int sizeIS)
+	{
+		super(unsortedIS,sizeIS);
+		this.sorterName = "Insertion ";
+	}
+	
+	public void sort() //just sorts given array
+	{
+		for (int i=1;i<size;i++)
+		{
+			int key = unsorted[i];
+			int j = i-1;
+			while(j>=0 && unsorted[j]>key)
+			{
+				unsorted[j+1]=unsorted[j];
+				j--;
+			}
+			unsorted[j+1]=key;
+		}
+	}
+}
